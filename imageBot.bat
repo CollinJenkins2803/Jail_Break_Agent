177e86cde09f04a8f370201918ad406222ce4346
Collin Jenkins <cpj00003@mix.wvu.edu>
2024-09-28 18:34:08 -0400

    Jail_Break_Agent

--- imageBot.bat
+++ imageBot.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\cjenk\source\repos\imagebot\imagebot
+cd C:\Users\cjenk\source\repos\Jail_Break_Agent\Jail_Break_Agent
 
 :: Start the backend process
 start cmd /k "node index.js"
